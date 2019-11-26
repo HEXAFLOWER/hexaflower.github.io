44688185473ea3921c974c308f5a253de271766f
erzqy <riezqy189@gmail.com>
2019-11-26 11:07:07 +0800

    something

--- js/hexaflower.js
+++ js/hexaflower.js
@@ -2,17 +2,32 @@ function log(o){
   console.log(o);
 }
 
-function html5Support(tag = 'canvas') {
+function appendBody(element) {
 
-  return document.createElement(tag) ? true : false; 
+  return document.body.appendChild(element);
 }
 
-function playAudio(src, autoplay = false, loop = true, hidden = false, controls = true){
+function createElementAlt(tag) {
+
+  return document.createElement(tag);
+}
+
+function html5Support(tag = 'canvas') {
+
+  return createElementAlt(tag) ? true : false; 
+}
+
+function isChrome() {
+
+  return !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+}
+
+function playAudio(src, autoplay = false, loop = true, hidden = true, controls = true){
   id = 'hexaflower-player';
   if(html5Support()){
     audio = document.querySelector('#' + id);
     if (!audio) {
-      audio = document.createElement('audio');
+      audio = createElementAlt('audio');
     }
 
     audio.id = id;
@@ -24,15 +39,30 @@ function playAudio(src, autoplay = false, loop = true, hidden = false, controls 
     
   }
 
-  return document.body.appendChild(audio);
+  return appendBody(audio);
 }
 
-function playAudioRandom(list = audio_list.audios, autoplay = false, loop = true, hidden = false, controls = true) {
+function playAudioInit(src, autoplay = false, loop = true, hidden = true, controls = true) {
+  if (isChrome()) {
+    layer = createElementAlt('div');
+    layer.style = 'left: 0px; top: 0px;width: 100%; height: 100%; opacity: 0.3; background-color: #D8D8D8;position: fixed;';
+    layer.onclick = function () {
+      playAudio(src, autoplay , loop , hidden , controls );
+      this.remove();
+    };
+
+    return appendBody(layer);
+  } else {
+    return playAudio(src, autoplay , loop , hidden , controls ); 
+  }  
+}
+
+function playAudioRandom(list = audio_list.audios, autoplay = false, loop = true, hidden = true, controls = true) {
   i = Math.floor((Math.random() * list.length - 1) + 1);
   path = './audios/' + list[i];
   // alert('We use cookies. :kiss:');
 
-  if (playAudio(path, autoplay, loop, hidden, controls)) {
+  if (playAudioInit(path, autoplay, loop, hidden, controls)) {
     return true;
   } else {
     return false;
